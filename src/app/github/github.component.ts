cae51be3c317d96b6dd5d9fd72e3a119a1fce5ce
joan-w <wamuyujoan4@gmail.com>
2020-04-12 16:05:46 +0300

    Adding a github component

---
+++ src/app/github/github.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+// import { GithubService } from '../services/github.service';
+
+@Component({
+  selector: 'app-github',
+  templateUrl: './github.component.html',
+  styleUrls: ['./github.component.css']
+})
+export class GithubComponent implements OnInit {
+
+  // constructor(private githubService:GithubService){}
+
+  ngOnInit() {
+  }
+
+}
