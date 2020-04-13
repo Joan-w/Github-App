00dbcfb6ba45708c152874ffa4d8e9a06776c20e
joan-w <wamuyujoan4@gmail.com>
2020-04-13 23:33:20 +0300

    Writing an updated function that allows one to search their usernames

--- src/app/Services/profile.service.ts
+++ src/app/Services/profile.service.ts
@@ -16,6 +16,18 @@ export class ProfileService {
   }
 
   getProfileInfo(){
-    return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret);
+    return this.http.get("https://api.github.com/users/" + 
+    this.username + "?client_id=" + this.clientid + "&client_secret=" 
+    + this.clientsecret);
+  }
+
+  getProfileRepos(){
+    return this.http.get("https://api.github.com/users/" + 
+    this.username + "/repos?client_id=" + this.clientid + "&client_secret=" 
+    + this.clientsecret);
+  }
+
+  updateProfile(username:string){
+    this.username = username;
   }
 }
