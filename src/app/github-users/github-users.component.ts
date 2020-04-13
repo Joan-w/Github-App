49328189c5274f30e143dcaa4cbfe87daccc55c2
joan-w <wamuyujoan4@gmail.com>
2020-04-13 14:04:55 +0300

    Importing a UserService service and injecting it in the constructor

--- src/app/github-users/github-users.component.ts
+++ src/app/github-users/github-users.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { UserService } from "../user.service";
 
 @Component({
   selector: 'app-github-users',
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubUsersComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   ngOnInit() {
   }
