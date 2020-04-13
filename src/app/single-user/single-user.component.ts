34c8967aed5fe953dbfe1f76f1ade980a8a3e741
joan-w <wamuyujoan4@gmail.com>
2020-04-13 20:25:23 +0300

    Importing the activated route and injecting it in the constructor as a route

--- src/app/single-user/single-user.component.ts
+++ src/app/single-user/single-user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-single-user',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SingleUserComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
   }
