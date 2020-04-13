50a662e06482d899cb9f9c2770460310a3676473
joan-w <wamuyujoan4@gmail.com>
2020-04-13 18:49:12 +0300

    Not a commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
-
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-// import { UserService } from './user.service';
-import { GithubUsersComponent } from './github-users/github-users.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    GithubUsersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
   ],
   providers: [
-    // UserService
   ],
   bootstrap: [AppComponent]
 })
--- src/app/users.ts
+++
@@ -1,3 +0,0 @@
-export class Users {
-    constructor(public username:string, public user:string, public repos:string){}
-}
