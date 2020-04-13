b867957447f3a6fca5630ea7325329eca9b543ed
joan-w <wamuyujoan4@gmail.com>
2020-04-13 22:01:02 +0300

    Importing the service file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,26 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
-import { NgProgressModule } from '@ngx-progressbar/core';
-import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
-import { AppRoutingModule } from './app-routing.module';
 
-import { UserService } from "./user.service";
+import { ProfileService } from "./Services/profile.service";
+
 import { AppComponent } from './app.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    HttpClientModule,
-    NgProgressModule.forRoot(),
-    NgProgressHttpClientModule
+    BrowserModule
   ],
   providers: [
-    UserService
+    ProfileService
   ],
   bootstrap: [AppComponent]
 })
