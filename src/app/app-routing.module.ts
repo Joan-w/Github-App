802ab101df52fa0d2a20030ff38609d62b13a721
joan-w <wamuyujoan4@gmail.com>
2020-04-13 20:21:01 +0300

    Importing Github users and single uuser component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GithubUsersComponent } from "./Github-User/github-users/github-users.component";
+import { SingleUserComponent } from "./single-user/single-user.component";
 
 
 const routes: Routes = [
+  { path: '', component: GithubUsersComponent },
+  { path: ':username', component: SingleUserComponent }
 ];
 
 @NgModule({
