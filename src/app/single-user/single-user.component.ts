fd25b1244bd7c8dd3875cb2299cb5711f941303b
joan-w <wamuyujoan4@gmail.com>
2020-04-13 20:30:04 +0300

    Importing the userservice service and injecting it in the constructor

--- src/app/single-user/single-user.component.ts
+++ src/app/single-user/single-user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { UserService } from "../user.service";
 
 @Component({
   selector: 'app-single-user',
@@ -8,7 +9,9 @@ import { ActivatedRoute } from "@angular/router";
 })
 export class SingleUserComponent implements OnInit {
 
-  constructor(private route: ActivatedRoute) { }
+  user;
+
+  constructor(private route: ActivatedRoute, private userService: UserService) { }
 
   ngOnInit() {
     this.route.params.subscribe(params => {
