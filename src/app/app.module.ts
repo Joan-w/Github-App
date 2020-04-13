1a716232095aa4cf26148d37eaab55b17e009f70
joan-w <wamuyujoan4@gmail.com>
2020-04-13 23:29:29 +0300

    Importing a forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { ProfileService } from "./Services/profile.service";
+import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
@@ -13,11 +14,10 @@ import { ProfileComponent } from './profile/profile.component';
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
-  providers: [
-    ProfileService
-  ],
+  providers: [ProfileService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
