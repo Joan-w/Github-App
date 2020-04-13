01a6f40dd90c8727fd5daffdd70e47d1733f884e
joan-w <wamuyujoan4@gmail.com>
2020-04-13 19:59:30 +0300

    Grabbing data in the ngOnInit and adding a users property in the class

--- src/app/Github-User/github-users/github-users.component.ts
+++ src/app/Github-User/github-users/github-users.component.ts
@@ -7,10 +7,13 @@ import { UserService } from "../../user.service";
   styleUrls: ['./github-users.component.css']
 })
 export class GithubUsersComponent implements OnInit {
+  
+  users;
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
+    this.users = this.userService.getUsers();
   }
 
 }
