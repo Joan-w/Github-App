c42a92dde57ad4eccb76405b7ad756f50158e935
joan-w <wamuyujoan4@gmail.com>
2020-04-13 23:32:06 +0300

    Writing a findProfile function that allows one to search their username and get their profile

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -8,12 +8,24 @@ import { ProfileService } from "../Services/profile.service";
 })
 export class ProfileComponent implements OnInit {
   profile:any;
+  repos:any;
+  username:string;
 
   constructor(private profileService: ProfileService) { 
+    
+  }
+
+  findProfile(){
+    this.profileService.updateProfile(this.username);
     this.profileService.getProfileInfo().subscribe(profile =>{
       console.log(profile);
       this.profile = profile;
     });
+
+    this.profileService.getProfileRepos().subscribe(repos => {
+      console.log(repos);
+      this.repos = repos;
+    })
   }
 
   ngOnInit() {
