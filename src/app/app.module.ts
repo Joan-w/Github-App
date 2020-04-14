6a2996d8895c26f92415382d2beac800809018c9
joan-w <wamuyujoan4@gmail.com>
2020-04-14 10:46:41 +0300

    Importing the RouterModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,27 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule } from "@angular/common/http";
 import { ProfileService } from "./Services/profile.service";
 import { FormsModule } from "@angular/forms";
+import { RouterModule, Routes } from '@angular/router';
 
+import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from './app.component';
 import { ProfileComponent } from './profile/profile.component';
+import { LandingPageComponent } from './landing-page/landing-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent
+    ProfileComponent,
+    LandingPageComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {path: '', component: LandingPageComponent},
+      {path: 'profile', component: ProfileComponent}
+    ])
   ],
   providers: [ProfileService],
   bootstrap: [AppComponent]
